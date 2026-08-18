13d2ed719a6c933495fe57b2848766491ba859ea
Croissant215 <croissant215@gmail.com>
2026-08-18 22:48:59 +0900

    Fix UI bindings for quota reset times and add Weekly limit unavailable message

--- obj/Release/net9.0-windows/AgyUsageShower.AssemblyInfo.cs
+++ obj/Release/net9.0-windows/AgyUsageShower.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgyUsageShower")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6469cec251a406bcac1eab67c3e8df70c7758b0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a2ee9040e553406d8d1fd9f647fccbf42e2e6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgyUsageShower")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgyUsageShower")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
